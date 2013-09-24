6272cbc52462af2e9ee227a307fea6fbebd00791
M Teguh A Suandi <teguh.andro@gmail.com>
2013-09-24 12:49:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 include_once('GoogleCustomSearch.class.php');
-$apikey 	= 'AIzaSyCBB00XLGn12GeK39PrImye1q23x9vi0I0';
-$seid 		= '018407028706153065349:v9jun2he5gc';
-$keyword 	= urlencode('RIM');
+$apikey 	= '';
+$seid 		= '';
+$keyword 	= urlencode('');
 
 $api 	= new GoogleCustomSearch();
 $req 	= $api->customSearch($apikey, $seid, $keyword);
